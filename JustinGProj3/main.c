207efe95bdba6a919363d0cd25931b1581598186
justingarcia <justgarcia5@gmail.com>
2021-04-10 14:06:49 -0700

    Initial Commit

---
+++ JustinGProj3/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  JustinGProj3
+//
+//  Created by Justin Garcia on 4/10/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
